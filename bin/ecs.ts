b2b2116da87a1c0eb725e3f8e82668526a28a231
Laurentiu Daniel Ginghina <laurica@Laurentius-MacBook-Pro.local>
2024-09-03 11:16:10 +0300

    wip

--- bin/ecs.ts
+++ bin/ecs.ts
@@ -9,6 +9,11 @@ import { EcsStack } from '../lib/ecs-stack';
 
 const app = new cdk.App();
 
+const env = { 
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+};
+
 new ContainerRepositoryStack(app, 'ContainerRepositoryStack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
@@ -16,19 +21,21 @@ new ContainerRepositoryStack(app, 'ContainerRepositoryStack', {
   },
 });
 
-const networkStack = new NetworkingStack(app, 'NetworkingStack', {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
-});
+// Create the networking stack
+const networkStack = new NetworkingStack(app, 'NetworkingStack', { env });
 
-new EcsStack(app, 'EcsStack', {
+// Create the ECS stack
+const ecsStack = new EcsStack(app, 'EcsStack', {
   vpc: networkStack.pocVpc,
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
+  env: env,
 });
 
-EcsStack.addDependency(NetworkingStack);
+// Correctly add the dependency
+ecsStack.addDependency(networkStack);
+
+// Add tags to all resources in both stacks
+cdk.Tags.of(networkStack).add('Project', 'MyPoC');
+cdk.Tags.of(ecsStack).add('Project', 'MyPoC');
+
+app.synth();
+
