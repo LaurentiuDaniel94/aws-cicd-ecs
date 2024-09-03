3b9936ba65438b01577c33960ae104198dd8cca6
Laurentiu Daniel Ginghina <laurica@Laurentius-MacBook-Pro.local>
2024-09-03 13:29:50 +0300

    wip

--- lib/ecs-stack.ts
+++ lib/ecs-stack.ts
@@ -6,76 +6,57 @@ import * as ecr from 'aws-cdk-lib/aws-ecr';
 import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 
 interface EcsStackProps extends cdk.StackProps {
-    vpc: ec2.IVpc;
-    repository: ecr.IRepository;
+  vpc: ec2.IVpc;
+  repository: ecr.IRepository;
 }
 
 export class EcsStack extends cdk.Stack {
-    constructor(scope: Construct, id: string, props: EcsStackProps) {
-      super(scope, id, props);
+  constructor(scope: Construct, id: string, props: EcsStackProps) {
+    super(scope, id, props);
 
-      const pocCluster = new ecs.Cluster(this, 'PocCluster', {
-        clusterName: 'poc-ecs-cluster',
-        vpc: props.vpc
-      });
-
-      pocCluster.addCapacity('DefaultAutoScalingGroup', {
-        instanceType: new ec2.InstanceType('t2.micro'),
-        minCapacity: 1,
-        maxCapacity: 3,
+    const cluster = new ecs.Cluster(this, 'DemoCluster', {
+      vpc: props.vpc
     });
 
-      const pocTaskDefinition = new ecs.Ec2TaskDefinition(this, 'PocTaskDef',{
-        networkMode: ecs.NetworkMode.BRIDGE,
-        
-      })
-      const pocContainer = pocTaskDefinition.addContainer('pocContainer', {
-        image: ecs.ContainerImage.fromEcrRepository(props.repository),
-        memoryLimitMiB: 256,
-        cpu: 256,
-        logging: new ecs.AwsLogDriver({
-            streamPrefix: 'pocContainer',
-            }),
-      });
+    const taskDefinition = new ecs.FargateTaskDefinition(this, 'DemoTaskDef', {
+      memoryLimitMiB: 512,
+      cpu: 256,
+    });
 
-        pocContainer.addPortMappings({
-            containerPort: 80,
-            hostPort: 80,
-            protocol: ecs.Protocol.TCP,
-        });
+    const container = taskDefinition.addContainer('DemoContainer', {
+      image: ecs.ContainerImage.fromEcrRepository(props.repository, 'latest'),
+      logging: ecs.LogDrivers.awsLogs({ streamPrefix: 'DemoContainer' }),
+    });
 
-        const pocService = new ecs.Ec2Service(this, 'PocService', {
-            cluster: pocCluster,
-            taskDefinition: pocTaskDefinition,
-        });
+    container.addPortMappings({
+      containerPort: 80,
+      protocol: ecs.Protocol.TCP,
+    });
 
-        //create a load balancer
-        pocService.connections.allowFromAnyIpv4(ec2.Port.tcp(80));
-        pocService.connections.allowFromAnyIpv4(ec2.Port.tcp(443));
+    const service = new ecs.FargateService(this, 'DemoService', {
+      cluster,
+      taskDefinition,
+      desiredCount: 2,
+    });
 
-        const pocLoadBalancer = new elbv2.ApplicationLoadBalancer(this, 'PocLoadBalancer', {
-            vpc: props.vpc,
-            internetFacing: true,
-        });
+    const lb = new elbv2.ApplicationLoadBalancer(this, 'DemoLB', {
+      vpc: props.vpc,
+      internetFacing: true
+    });
 
-        const listener = pocLoadBalancer.addListener('Listener', {
-            port: 80,
-            open: true
-        });
+    const listener = lb.addListener('DemoListener', {
+      port: 80,
+    });
 
-        listener.addTargets('ECS', {
-            port: 80,
-            targets: [pocService.loadBalancerTarget({
-                containerName: 'pocContainer',
-                containerPort: 80,
-            })],
-            healthCheck: {
-                interval:cdk.Duration.seconds(60),
-                path: '/',
-                timeout: cdk.Duration.seconds(5),
-            }
-        });
+    listener.addTargets('DemoTarget', {
+      port: 80,
+      targets: [service],
+      healthCheck: {
+        path: '/',
+        interval: cdk.Duration.seconds(60),
+      },
+    });
 
-
-    }
+    new cdk.CfnOutput(this, 'LoadBalancerDNS', { value: lb.loadBalancerDnsName });
+  }
 }
---
+++ lib/pipeline-stack.ts
@@ -0,0 +1,108 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as codecommit from 'aws-cdk-lib/aws-codecommit';
+import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
+import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+
+interface PipelineStackProps extends cdk.StackProps {
+  ecrRepository: ecr.IRepository;
+  ecsService: ecs.IBaseService;
+}
+
+export class PipelineStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: PipelineStackProps) {
+    super(scope, id, props);
+
+    // Pipeline
+    const pipeline = new codepipeline.Pipeline(this, 'Pipeline', {
+      pipelineName: 'MyECSPipeline',
+    });
+
+    // Source Stage
+    const sourceOutput = new codepipeline.Artifact();
+    const sourceAction = new codepipeline_actions.GitHubSourceAction({
+      actionName: 'GitHub_Source',
+      owner: 'LaurentiuDaniel94',
+      repo: 'aws-typescript',
+      oauthToken: cdk.SecretValue.secretsManager('GITHUB_TOKEN'),
+      output: sourceOutput,
+      branch: 'main', // or your preferred branch
+    });
+
+    pipeline.addStage({
+      stageName: 'Source',
+      actions: [sourceAction],
+    });
+
+    // Build Stage
+    const buildOutput = new codepipeline.Artifact();
+    const buildProject = new codebuild.PipelineProject(this, 'BuildProject', {
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
+        privileged: true,
+      },
+      environmentVariables: {
+        REPOSITORY_URI: { value: props.ecrRepository.repositoryUri },
+      },
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          pre_build: {
+            commands: [
+              'echo Logging in to Amazon ECR...',
+              '$(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)',
+            ],
+          },
+          build: {
+            commands: [
+              'echo Build started on `date`',
+              'echo Building the Docker image...',
+              'docker build -t $REPOSITORY_URI:$CODEBUILD_RESOLVED_SOURCE_VERSION .',
+              'docker tag $REPOSITORY_URI:$CODEBUILD_RESOLVED_SOURCE_VERSION $REPOSITORY_URI:latest',
+            ],
+          },
+          post_build: {
+            commands: [
+              'echo Build completed on `date`',
+              'echo Pushing the Docker image...',
+              'docker push $REPOSITORY_URI:$CODEBUILD_RESOLVED_SOURCE_VERSION',
+              'docker push $REPOSITORY_URI:latest',
+              'echo Writing image definitions file...',
+              'printf \'[{"name":"CONTAINER_NAME","imageUri":"%s"}]\' $REPOSITORY_URI:$CODEBUILD_RESOLVED_SOURCE_VERSION > imagedefinitions.json',
+            ],
+          },
+        },
+        artifacts: {
+          files: ['imagedefinitions.json'],
+        },
+      }),
+    });
+
+    const buildAction = new codepipeline_actions.CodeBuildAction({
+      actionName: 'DockerBuild',
+      project: buildProject,
+      input: sourceOutput,
+      outputs: [buildOutput],
+    });
+
+    pipeline.addStage({
+      stageName: 'Build',
+      actions: [buildAction],
+    });
+
+    // Deploy Stage
+    const deployAction = new codepipeline_actions.EcsDeployAction({
+      actionName: 'DeployAction',
+      service: props.ecsService,
+      input: buildOutput,
+    });
+
+    pipeline.addStage({
+      stageName: 'Deploy',
+      actions: [deployAction],
+    });
+  }
+}
